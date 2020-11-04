e4082ce35e282b98b74351f733f1aaa8da968430
KIPKIPS <2432719656@qq.com>
2020-11-05 01:36:33 +0800

    提交新的字体和UIManager

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+    Transform endPanel;
+    Image fadeMask;
+    void Start() {
+        endPanel = GameObject.Find("FadeInOutMask").transform;
+        fadeMask = endPanel.GetComponent<Image>();
+        FindObjectOfType<Player>().OnDeath += OnGameOver;//触发game over事件
+    }
+
+    void Update() {
+
+    }
+    /// <summary>
+    /// 弹出game over面板
+    /// </summary>
+    void OnGameOver() {
+
+        endPanel.gameObject.SetActive(true);
+
+    }
+}
