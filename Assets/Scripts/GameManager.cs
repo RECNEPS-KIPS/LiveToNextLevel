d6d4bdcb180aa3be12da4f078bde1e29e9945fc6
KIPKIPS <2432719656@qq.com>
2020-11-08 19:18:19 +0800

    实现单例基类

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : BaseSingleton<GameManager> {
+    bool isGMPanelShow = false;
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.G)) {
+            if (!isGMPanelShow) {
+                print("show gm panel");
+            } else {
+                print("hide gm panel");
+            }
+        }
+    }
+}
