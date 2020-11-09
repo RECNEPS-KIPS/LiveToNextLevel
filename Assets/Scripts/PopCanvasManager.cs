88fea2da76c03b913a5106ec89e3ae0126e82504
KIPKIPS <2432719656@qq.com>
2020-11-10 03:05:47 +0800

    浮动Tips的实现
    用于做使用道具,切换武器等短暂的提示信息

--- Assets/Scripts/PopCanvasManager.cs
+++ Assets/Scripts/PopCanvasManager.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class PopCanvas : MonoBehaviour {
+public class PopCanvasManager : MonoBehaviour { //BaseSingleton<PopCanvasManager>
     Transform fadePanel;
     Transform endPanel;
     Image fadeMask;
     Color fadePanelColor;
     Button againBtn;
+    Transform popMessageTrs;
     void Start() {
+        popMessageTrs = Utils.FindObj<Transform>(transform, "PopMessageTrs");
         fadePanel = transform.Find("FadeInOutMask").transform;
         fadeMask = fadePanel.GetComponent<Image>();
         fadePanelColor = fadeMask.color;
@@ -32,9 +34,10 @@ public class PopCanvas : MonoBehaviour {
         //DontDestroyOnLoad(popCavas.gameObject);
         //print("Game Over");
         fadePanel.gameObject.SetActive(true);
+        print("game over");
         Color curColor = new Color(fadePanelColor.r, fadePanelColor.g, fadePanelColor.b, 0);
         Color tarColor = new Color(fadePanelColor.r, fadePanelColor.g, fadePanelColor.b, 1);
-        StartCoroutine(Fade(curColor, tarColor, 1f));
+        StartCoroutine(GameOverPanelFade(curColor, tarColor, 1f));
 
     }
     /// <summary>
@@ -44,7 +47,7 @@ public class PopCanvas : MonoBehaviour {
     /// <param name="tar">目标颜色</param>
     /// <param name="time">渐显时间</param>
     /// <returns></returns>
-    IEnumerator Fade(Color cur, Color tar, float time) {
+    IEnumerator GameOverPanelFade(Color cur, Color tar, float time) {
         float speed = 1 / time;
         float percent = 0;
         while (percent < 1) {
@@ -60,6 +63,40 @@ public class PopCanvas : MonoBehaviour {
         //print("play again");
         SceneManager.LoadScene("Game");
         GameManager.Instance.isPlayerDeath = false;
-        //fadePanel.gameObject.SetActive(false);
+
+        fadePanel.gameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// 上浮弹出框
+    /// </summary>
+    /// <param name="pos">弹出框的位置</param>
+    /// <param name="msg">弹出框展示的文本</param>
+    /// <param name="color">文本色号</param>
+    public void PopMessage(Vector3 pos, string msg, Color color) {
+        GameObject pop = Instantiate<GameObject>(Resources.Load<GameObject>("Prefabs/UI/PopMessagePanel"), popMessageTrs);
+        //print(popMessageTrs == null);
+        pop.transform.localPosition = pos;
+        string hexColor = "ffffff";
+        string addColorMsg = "<color=#" + hexColor + ">" + msg + "</color>";
+        Text txt = Utils.FindObj<Text>(pop.transform, "Text");
+        txt.text = addColorMsg;
+
+        //fade out
+        CanvasGroup canvas = Utils.FindObj<CanvasGroup>(pop.transform, null);
+        StartCoroutine(PopMessagePanelFadeAndPop(pop, canvas, 1f, 50));
+    }
+    IEnumerator PopMessagePanelFadeAndPop(GameObject pop, CanvasGroup canvas, float fadeTime, float moveSpeed) {
+        float alphaPercent = 1;
+        float y = 0;
+        Destroy(pop, fadeTime + 0.5f);
+        float fadeSpeed = 1 / fadeTime;
+        while (alphaPercent > 0) {
+            y += Time.deltaTime * fadeSpeed * moveSpeed;
+            pop.transform.localPosition = new Vector3(0, y, 0);
+            alphaPercent -= Time.deltaTime * fadeSpeed;
+            canvas.alpha = Mathf.Lerp(0, 1, alphaPercent);
+            yield return null;
+        }
     }
 }
