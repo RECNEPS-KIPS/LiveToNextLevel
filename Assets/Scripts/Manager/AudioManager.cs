3e308a78943747420ff56a3e7ffd31ed600ba212
KIPKIPS <2432719656@qq.com>
2020-11-26 00:40:42 +0800

    音频的处理

--- Assets/Scripts/Manager/AudioManager.cs
+++ Assets/Scripts/Manager/AudioManager.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start() {
+    float mainVolumePercent = 1;//主音量百分比
+    float sfxVolumePercent = 1;//特技效果(Special Effects Cinematography)音量占比
+    float musicVolumePercent = 1;//音乐音量占比
+
+    //使用多个音频轨道,以便于在多个音轨上淡入淡出
+    AudioSource[] musicSources;
+    int activeMusicSourceIndex;//激活的音轨
+
+    void Awake() {
+        musicSources = new AudioSource[2];
+        for (var i = 0; i < 2; i++) {
+            GameObject newMusicSource = new GameObject("Music Source " + (i + 1));//创建游戏对象
+            musicSources[i] = newMusicSource.AddComponent<AudioSource>();
+            newMusicSource.transform.parent = transform;
+        }
+    }
+
+    //淡入淡出音轨
+    public void PlayMusic(AudioClip clip, float fadeDuration = 1) {
 
     }
 
-    // Update is called once per frame
-    void Update() {
-
+    //播放音频文件 在pos位置
+    public void PlayAudioClip(AudioClip clip, Vector3 pos) {
+        AudioSource.PlayClipAtPoint(clip, pos, sfxVolumePercent * mainVolumePercent);
     }
 }
