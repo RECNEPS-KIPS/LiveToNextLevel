7e1ecbbcea2226e19e84d335c8d11b4652f2c3e4
KIPKIPS <2432719656@qq.com>
2020-11-30 00:54:11 +0800

    UIPanelType枚举类,保存UI界面的类型

---
+++ Assets/UIFramework/UIPanelType.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UIPanelType {
+    PopTips, //弹出提示tips
+    PopPanel, //弹出窗口
+    MainUI, //主界面UI
+    CommonPanel, //通用窗口
+}
