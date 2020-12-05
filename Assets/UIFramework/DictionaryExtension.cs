11c7188b188410dbd49c78343ea2e9e5931a185a
KIPKIPS <2432719656@qq.com>
2020-12-05 22:58:19 +0800

    字典扩展类

---
+++ Assets/UIFramework/DictionaryExtension.cs
@@ -0,0 +1,15 @@
+﻿// author:KIPKIPS
+// time:2020.12.05 18:31:52
+// describe:BasePanel 字典扩展方法类
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DictionaryExtension {
+    //获取字典中key对应的value值,得到返回key 否则null
+    public static Tvalue TryGet<Tkey, Tvalue>(this Dictionary<Tkey, Tvalue> dict, Tkey key) {
+        Tvalue value;
+        dict.TryGetValue(key, out value);
+        return value;
+    }
+}
