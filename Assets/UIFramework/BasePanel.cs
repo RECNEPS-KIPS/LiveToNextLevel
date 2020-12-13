0a72712ad4a85715717c9dad33d41007609d5eea
KIPKIPS <2432719656@qq.com>
2020-12-13 16:17:28 +0800

    引入DoTween

--- Assets/UIFramework/BasePanel.cs
+++ Assets/UIFramework/BasePanel.cs
@@ -6,6 +6,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class BasePanel : MonoBehaviour {
     // Start is called before the first frame update
